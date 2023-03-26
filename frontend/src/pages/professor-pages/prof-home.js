cc477cf8289996782b0d8e25d1c554b50aa0b1ec
Rachel Peterson <rachel.peterson0804@gmail.com>
2023-03-26 11:39:38 -0400

    add backbone of student dashboard, make answer choices display correct/incorrect footer

--- frontend/src/pages/professor-pages/prof-home.js
+++ frontend/src/pages/professor-pages/prof-home.js
@@ -3,15 +3,11 @@ import Button from '@mui/material/Button';
 import Header from '../../components/header';
 import '../../App.css'; 
 import { Link } from "react-router-dom";
-import LinearProgress from '@mui/material/LinearProgress';
 
 function ProfHome() {
     return (
         <div>
             <Header />
-            <div class="progress-bar">
-                <LinearProgress variant="determinate" value={50} />     
-            </div>
             
             <h1 className="title">Professor Home Page</h1>
 
--- frontend/src/pages/student-pages/student-home.js
+++ frontend/src/pages/student-pages/student-home.js
@@ -1,27 +1,17 @@
 import React from 'react'
-import Button from '@mui/material/Button';
 import Header from '../../components/header';
 import '../../App.css'; 
-import { Link } from "react-router-dom";
-import LinearProgress from '@mui/material/LinearProgress';
+import Spanish1Dashboard from '../../components/spanish1_dash';
+
 
 function StudentHome() {
     return (
         <div>
             <Header />
-            <div class="progress-bar">
-                <LinearProgress variant="determinate" value={50} />     
-            </div>
-            
-            <h1 className="title">Student Home Page</h1>
+            <br/>
 
-            <div className="button-container">
-                <div className="button">
-                    <Button variant="contained">
-                        <Link to="/class-selection" class="link">Testing: Class Selection Page</Link>
-                    </Button>
-                </div>
-            </div>
+            {/* Check student's registered class and display correct class page here  */}
+            <Spanish1Dashboard />
         </div>
 
         
