c7c4dfd5c347ce0451c48dc5d1bb59659d865c89
Matthew Davis <matthewpalmerdavis@gmail.com>
2023-04-05 12:21:27 -0400

    Implemented some login/logout functionality

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,65 +1,80 @@
-import React, {} from 'react'
-import Button from '@mui/material/Button';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Header from '../components/header';
+import React from "react";
+import Button from "@mui/material/Button";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Header from "../components/header";
 import { Link } from "react-router-dom";
-import '../App.css'; 
-
+import "../App.css";
 
 const Home = () => {
-    const theme = createTheme({
-        palette: {
-          primary: {
-            main: '#FFFFFF',
-          },
-        },
-      });
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#FFFFFF",
+      },
+    },
+  });
 
-    return <div class="home">
-        {/* Header */}
-        <Header /> 
-        <div class="main-container">
-            <h1> Welcome to GatorLingo </h1>
-            <p> Your favorite study guide for your language courses at University of Florida. </p>
-            <div className='main-btns'>
-                <Link to="/login" class="link">
-                    <ThemeProvider theme={theme}>
-                    <Button color="primary" variant="outlined" size="large" sx={{m: 2}}>
-                        GET STARTED
-                    </Button>
-                    </ThemeProvider>
-                </Link>
+  return (
+    <div class="home">
+      {/* Header */}
+      <Header />
+      <div class="main-container">
+        <h1> Welcome to GatorLingo </h1>
+        <p>
+          {" "}
+          Your favorite study guide for your language courses at University of
+          Florida.{" "}
+        </p>
+        <div className="main-btns">
+          <Link to="/login" class="link">
+            <ThemeProvider theme={theme}>
+              <Button
+                color="primary"
+                variant="outlined"
+                size="large"
+                sx={{ m: 2 }}
+              >
+                GET STARTED
+              </Button>
+            </ThemeProvider>
+          </Link>
 
-                <Link to="/" class="link">
-                    <ThemeProvider theme={theme}>
-                    <Button color="primary" variant="contained" size="large" fontSize="60">
-                        ABOUT US
-                    </Button>
-                    </ThemeProvider>
-                </Link>
-            </div>
+          <Link to="/" class="link">
+            <ThemeProvider theme={theme}>
+              <Button
+                color="primary"
+                variant="contained"
+                size="large"
+                fontSize="60"
+              >
+                ABOUT US
+              </Button>
+            </ThemeProvider>
+          </Link>
         </div>
+      </div>
 
-        {/* Title */}
-        <h1 class="title">
-            Welcome to GatorLingo
-        </h1>
+      {/* Title */}
+      <h1 class="title">Welcome to GatorLingo</h1>
 
-        <div className="button-container">
-            <div class="button">
-                <Button variant="contained">
-                    <Link to="/student-home" class="link">Testing: Student Home Page</Link>
-                </Button>
-            </div>
-            <div className="button">
-                <Button variant="contained">
-                    <Link to="/prof-home" class="link">Testing: Professor Home Page</Link>
-                </Button>
-            </div>
+      <div className="button-container">
+        <div class="button">
+          <Button variant="contained">
+            <Link to="/student-home" class="link">
+              Testing: Student Home Page
+            </Link>
+          </Button>
         </div>
-        
+        <div className="button">
+          <Button variant="contained">
+            <Link to="/prof-home" class="link">
+              Testing: Professor Home Page
+            </Link>
+          </Button>
+        </div>
+      </div>
     </div>
-  };
-  
-  export default Home;
+  );
+};
 
+export default Home;
