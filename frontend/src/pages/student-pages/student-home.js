2d08a2d64bc0c6cf58fcb76f150f42cb7cfccd1b
Jonathan Cannella <Jonathan.cannella@gmail.com>
2023-04-06 16:02:52 -0400

    student dashboard update
    dashboard only displays if a user is logged in and also will have students name on it

--- frontend/src/pages/student-pages/student-home.js
+++ frontend/src/pages/student-pages/student-home.js
@@ -1,21 +1,50 @@
 import React from 'react'
 import Header from '../../components/header';
-import '../../App.css'; 
+import '../../App.css';
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "../../firebase-config";
 import Spanish1Dashboard from '../../components/spanish1_dash';
+import { Card } from '@mui/material';
 
+
+//console.log(user.displayName);
 
 function StudentHome() {
-    return (
-        <div>
-            <Header />
-            <br/>
+    const auth = getAuth();
+    const user = auth.currentUser;
 
-            {/* Check student's registered class and display correct class page here  */}
-            <Spanish1Dashboard />
-        </div>
+    if (user) {
+        console.log(user.displayName)
+        return (
+            <div>
+                <Header />
+                <br/>
+    
+                {/* Check student's registered class and display correct class page here  */}
+                <Spanish1Dashboard />
+            </div>
+    
+            
+        );
+    }
+    else {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+    // return (
+    //     <div>
+    //         <Header />
+    //         <br/>
+
+    //         {/* Check student's registered class and display correct class page here  */}
+    //         <Spanish1Dashboard />
+    //     </div>
 
         
-    );
+    // );
 }
 
 export default StudentHome;
